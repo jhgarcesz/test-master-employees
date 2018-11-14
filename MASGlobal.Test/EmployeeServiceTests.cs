ccffb3827463bf3f008c86230517695e6f6e5c76
jhgarcesz <jhgarcesz@gmail.com>
2018-11-14 17:55:22 -0500

    include unit test

---
+++ MASGlobal.Test/EmployeeServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using MASGlobal.Domain.Contracts;
+using MASGlobal.Domain.Domain;
+using MASGlobal.Domain.Dtos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MASGlobal.Tests
+{
+    [TestClass]
+    public class EmployeeServiceTests
+    {
+        private Mock<IEmployeeFactory> mockedFactory;
+        private Mock<IEmployeeRepository> mockedRepository;
+        private EmployeeService employeeService;
+        
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockedFactory = new Mock<IEmployeeFactory>();
+            mockedRepository = new Mock<IEmployeeRepository>();
+
+            mockedFactory.Setup(x => x.GenerateEmployeeDto(It.IsAny<BaseEmployeeDto>()))
+                .Returns(() => new EmployeeDto
+                {
+                    Id = 1234,
+                    Name = "Jhorx Garces"
+                });
+
+            employeeService = new EmployeeService(mockedRepository.Object, mockedFactory.Object);
+        }
+
+        [TestMethod]
+        public async Task EmployeeService_GetAllEmployees_WillReturnData()
+        {
+            //Arrange
+            mockedRepository.Setup(x => x.GetAllEmployees())
+                .ReturnsAsync(() => new List<BaseEmployeeDto>()
+                {
+                    new BaseEmployeeDto
+                    {
+                        Id = 234,
+                        Name = "Michael"
+                    },
+                    new BaseEmployeeDto
+                    {
+                        Id = 098,
+                        Name = "Peter"
+                    }
+                });
+
+            //Act
+            var result = await employeeService.GetAllEmployees();
+
+            //Assert
+            Assert.AreEqual(result.Count(), 2);
+        }
+    }
+}
---
+++ MASGlobal.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MASGlobal.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MASGlobal.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b84c5999-9a1e-45df-a3a5-6d2faffcd007")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
